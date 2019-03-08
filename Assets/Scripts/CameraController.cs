ca842793ff60f9670ebd5c823449aa2486ae24a7
Adrian De Andrade <adriandeandrade98@gmail.com>
2019-03-07 22:01:49 -0500

    stuff

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float cameraMoveSpeed = 120f;
+    [SerializeField] private float clampAngle = 80f;
+    [SerializeField] private float sensitivity = 150f;
+    [SerializeField] private float fovLerpSpeed = 0.5f;
+    [SerializeField] private float followSpeed = 0.5f;
+    [SerializeField] private bool doLerpTarget;
+    [SerializeField] private GameObject followTarget;
+    [SerializeField] private Vector3 offset;
+
+    float rotY;
+    float rotX;
+    float newFov;
+
+    bool isZoomed;
+
+    Camera cam;
+
+    private void Awake()
+    {
+        cam = Camera.main;
+    }
+
+    private void Start()
+    {
+        isZoomed = false;
+
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotY = rot.y;
+        rotX = rot.x;
+        newFov = cam.fieldOfView;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        float inputX = Input.GetAxis("Mouse X"); // Get input axises
+        float inputY = Input.GetAxis("Mouse Y");
+
+        rotY += inputX * sensitivity * Time.deltaTime; // Increment / decrement rotation values on x and y axis
+        rotX += -inputY * sensitivity * Time.deltaTime;
+
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0f);
+        transform.rotation = localRotation;
+
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, newFov, Time.deltaTime / fovLerpSpeed);
+    }
+
+    private void LateUpdate()
+    {
+        Transform target = followTarget.transform;
+
+        if (!isZoomed && doLerpTarget)
+        {
+            float step = followSpeed * Time.deltaTime;
+            transform.position = Vector3.Lerp(transform.position, target.position + offset, step);
+        }
+        else
+        {
+            float step = cameraMoveSpeed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.position + offset, step);
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,5 +5,57 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
-    
+    [SerializeField] private float jumpAmount;
+    [SerializeField] private float gravity;
+
+    Vector3 movement;
+    bool canDoubleJump;
+    int doubleJumpAmount = 2;
+    int doubleJumpCount;
+
+    CharacterController characterController;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        if (characterController.isGrounded)
+        {
+
+            Vector3 movementInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            movement = transform.TransformDirection(movementInput);
+            movement *= moveSpeed;
+
+            if (Input.GetButton("Jump"))
+            {
+                movement.y = jumpAmount;
+            }
+        }
+
+        movement.y = movement.y - (gravity * Time.deltaTime);
+
+        characterController.Move(movement * Time.deltaTime);
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit other)
+    {
+        if(other.gameObject.layer == LayerMask.NameToLayer("Ground") && !characterController.isGrounded)
+        {
+            Debug.Log("Hit ground");
+            canDoubleJump = true;
+        }
+    }
 }
